4602d743fb93fd67dec72c354af2e9a9afc808a1
RyanFahim <rfahim098@gmail.com>
2022-11-22 23:24:53 +0600

    Tuesday Home

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -27,19 +27,32 @@ const Services = () => {
                 </div>
 
                 <div className="service__right">
-                    <div className="exam">
-                        deaade
-                    </div>
-                   
+
+
                     <div className="services__menu">
-                        <div className="services__menuItem">Making Records</div>
+                        <div className="services__menuItem services__menuItem-main">Making Records & Cassettles</div>
                         <div className="services__menuItem">Data and Customization</div>
                         <div className="services__menuItem">Mastering</div>
                         <div className="services__menuItem">Artwork Assitance</div>
                         <div className="services__menuItem">Vinyl Pressing & Cassette Duplication</div>
+
+                        <div className="services__menuItem services__menuItem-main">Storage & Distribution</div>
+                        <div className="services__menuItem">Free Storages</div>
+                        <div className="services__menuItem">Sell with Qrates</div>
+                        <div className="services__menuItem">Digital Downloads</div>
+                        <div className="services__menuItem">Reatil Distributions</div>
                     </div>
                 </div>
             </div>
+
+            {/* Community section */}
+            <section className="container community__container">
+                <button className="commmunity__button">Community</button>
+                <div className="community__title">Join a growing community</div>
+                <p className="community__para">We're build a home for the next generation vinyl lovers and cassette junkies.
+                    Get in front of fans, connect with artists, labels and stores.</p>
+                <button className="community__read-stories">Read stories</button>
+            </section>
         </>
     )
 }
